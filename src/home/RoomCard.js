eb9b752f401792669df01615ce2158ddefef1b9b
borbeam18 <borbeam18@gmail.com>
2024-12-25 14:29:55 +0700

    Create RoomCard.js

---
+++ src/home/RoomCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';  // นำเข้า Link จาก react-router-dom
+import Rooms from '../components/Rooms';
+
+function RoomCard({ room, reviews }) {
+    return (
+        <div className="col-md-4 my-3">
+            <div className="card shadow-sm border-light">
+                <img 
+                    src={room.image} 
+                    className="card-img-top" 
+                    alt={room.name} 
+                />
+                <div className="card-body">
+                    <h5 className="card-title">{room.name}</h5>
+                    <p className="card-text">{room.description}</p>
+                    
+                    {/* ใช้ Link เพื่อให้ไปยังหน้าห้องพัก */}
+                    <Link to={`/rooms/${Rooms.id}`} className="btn btn-primary">จองเลย</Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default RoomCard;
